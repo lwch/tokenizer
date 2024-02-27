1998c13591f43659e854a093cce77f76cf6944c1
lwch <lwch748@gmail.com>
2024-02-27 15:13:53 +0800

    去除无用逻辑

--- dict.go
+++ dict.go
@@ -1,17 +1,11 @@
 package tokenizer
 
-import "math"
-
 type dict struct {
 	ch2id map[byte]int
 	id2ch []byte
 }
 
 func newDict(chars map[byte]struct{}) *dict {
-	// 中文常用字符约4000+，加上英文字符和数字，总数约不会超过65535
-	if len(chars) >= math.MaxUint16 {
-		panic("too many bytes")
-	}
 	d := &dict{
 		ch2id: make(map[byte]int, len(chars)),
 		id2ch: make([]byte, len(chars)+1),
